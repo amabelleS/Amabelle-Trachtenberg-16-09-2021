19ba9723e2441a7a13da9b322404479cf49b6e88
amabelle <amabelletr@gmail.com>
2021-09-16 19:09:16 +0300

    Basic routing config

---
+++ src/pages/Favorites/Favorites.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import * as S from './style';
+
+const Favorites = () => {
+  return (
+    <S.Favorites>
+      <S.Header>Favorites</S.Header>
+    </S.Favorites>
+  );
+};
+
+export default Favorites;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import Text from 'components/Text';
+import * as S from './style';
+
+const Home = () => {
+  return (
+    <S.Home>
+      <S.Content>
+        <S.Header>
+          {/* <Text size="64px" bold> */}
+          Weather App
+          {/* </Text> */}
+        </S.Header>
+      </S.Content>
+    </S.Home>
+  );
+};
+
+export default Home;
